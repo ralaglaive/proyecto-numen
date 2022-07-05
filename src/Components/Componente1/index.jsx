d0daea42f5d2a1c1f7aa08076ad5760e693742dc
hollowblind095 <hollowblind095@gmail.com>
2022-07-05 20:08:46 -0300

    first components

---
+++ src/Components/Componente1/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+
+const Componente1 = () => {
+  return (
+    <div>
+      <h2 className="title">Componente1</h2>
+    </div>
+  );
+};
+
+export default Componente1;
---
+++ src/Components/Componente3/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Componente3 = () => {
+  return (
+    <div>Componente3</div>
+  )
+}
+
+export default Componente3
